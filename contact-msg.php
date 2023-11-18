f63744f6a3ad4f9f346e7b3bbfd1d9e5d5956fd2
Helia Ghorbani <heliaghorbani2001@gmail.com>
2023-11-18 14:53:00 +0330

    add contact page

---
+++ contact-msg.php
@@ -0,0 +1,13 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Process the data as needed
+    // ...
+
+    // Redirect or display a success message
+}
+?>
